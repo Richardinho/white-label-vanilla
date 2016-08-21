98183a078869e425b97b1a5378816308a2d0f185
Richard Hunter <richard@richardhunter.co.uk>
2016-08-21 18:53:35 +0100

    changing port again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use('/js', express.static('js'));
 app.use('/selection-box', express.static('./bower_components/selection-box/build'));
